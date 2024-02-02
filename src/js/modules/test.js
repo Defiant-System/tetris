1339642cd25770e393b3f7c60a2afbc7a78bc257
Hakan Bilgin <hbi99@hotmail.com>
2024-02-02 13:52:06 +0100

    added countdown

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -3,11 +3,12 @@ let Test = {
 	init(APP) {
 		// return;
 		setTimeout(() => {
-			Game.setState("play");
+			window.find(".view-countdown").addClass("start");
+			// Game.setState("play");
 			// Game.setState("test", 2);
 
 			// setTimeout(() => FX.explode([[0,10,2],[1,10,3],[2,10,4]]), 300);
-		}, 500);
+		}, 1500);
 		// setTimeout(() => Game.setState("play"), 500);
 		// setTimeout(() => Game.setState("pause"), 1000);
 	}
